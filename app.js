9a8d8fdf020fe11755b32dce8af4a03cc77db00b
Matthew <Mattpet26@gmail.com>
2020-07-21 10:43:01 -0700

    small changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var productArray = [];
 var totalClicks = 0;
 var maxClicks = 25;
+var imageIndexCurrentlyDisplayed =[];
 
 // ================= Function Definitions =================
 function SkymallProduct(imageName, src){
@@ -28,6 +29,44 @@ SkymallProduct.prototype.renderImageHtml = function() {
 
   target.appendChild(busHomeLi);
 };
+
+
+function renderNewImages(){
+  var index1 = Math.floor(Math.random() * productArray.length);
+  while(index1 === imageIndexCurrentlyDisplayed[0] || index1 === imageIndexCurrentlyDisplayed[1]){
+    Math.floor(Math.random() * productArray.length);
+  }
+
+  var index2 = Math.floor(Math.random() * productArray.length);
+  while(
+    index1 === index2 || 
+    index2 === imageIndexCurrentlyDisplayed[0] || 
+    index2 === imageIndexCurrentlyDisplayed[1]
+   ) {
+      index2 = math.floor(Math.random() * productArray.length);
+  }
+
+  var index3 = Math.floor(Math.random() * productArray.length);
+  while(index2 === index3){
+    index3 = math.floor(Math.random() * productArray.length);
+  }
+
+  var newImage1 = productArray[index1];
+  var newImage2 = productArray[index2];
+  var newImage3 = productArray[index3];
+  // TODO: make that random
+  // create separate array with a while loop
+  var busList = document.getElementById('list-of-images');
+  busList.innerHTML = '';
+  newImage1.renderImageHtml();
+  newImage1.shown++;
+  newImage2.renderImageHtml();
+  newImage2.shown++;
+  newImage3.renderImageHtml();
+  newImage3.shown++;
+
+  imageIndexCurrentlyDisplayed[]
+}
 
 
 function handleClickOnImg(event) {
@@ -64,32 +103,13 @@ function renderVotesList(){
 }
 
 
-function renderNewImages(){
-  var index1 = Math.floor(Math.random() * productArray.length);
-  var index2 = Math.floor(Math.random() * productArray.length);
-  var index3 = Math.floor(Math.random() * productArray.length);
-
-  var newImage1 = productArray[index1];
-  var newImage2 = productArray[index2];
-  var newImage3 = productArray[index3];
-  // TODO: make that random
-  // create separate array with a while loop
-  var busList = document.getElementById('list-of-images');
-  busList.innerHTML = '';
-  newImage1.renderImageHtml();
-  newImage1.shown++;
-  newImage2.renderImageHtml();
-  newImage2.shown++;
-  newImage3.renderImageHtml();
-  newImage3.shown++;
-}
-
-
 var listOfImg = document.getElementById('list-of-images');
 listOfImg.addEventListener('click', handleClickOnImg);
 
 
-new SkymallProduct('Bathroom', 'images/bathroom.jpg');
-new SkymallProduct('Boots', 'images/boots.jpg');
+new SkymallProduct('R2-D2', 'images/bag.jpg');
+new SkymallProduct('Banana', 'images/banana.jpg');
+new SkymallProduct('Chair', 'images/chair.jpg');
 new SkymallProduct('Toasterboi', 'images/breakfast.jpg');
 new SkymallProduct('Bubbleyum', 'images/bubblegum.jpg');
+new SkymallProduct('Goodboi', 'images/dog-duck.jpg');
